fedef727ce4e51d7fec090619229265c7246c99c
Sandeep Agrahari <sandeepagrahari1911@gmail.com>
2026-03-06 22:26:49 +0530

    User Management System Project

---
+++ app/page.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { useState } from "react";
+import UserForm from "../components/UserForm";
+
+export default function Home() {
+  const [users, setUsers] = useState<any[]>([]);
+  const [search, setSearch] = useState("");
+  const [sortOrder, setSortOrder] = useState("asc");
+
+  const addUser = (user: any) => {
+    setUsers([...users, user]);
+  };
+  const sortUsers = () => {
+    const sorted = [...users].sort((a, b) => {
+      if (sortOrder === "asc") {
+        return a.name.localeCompare(b.name);
+      } else {
+        return b.name.localeCompare(a.name);
+      }
+    });
+
+    setUsers(sorted);
+
+    setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+  };
+  const deleteUser = (index: number) => {
+    const updatedUsers = users.filter((_, i) => i !== index);
+    setUsers(updatedUsers);
+  };
+
+  return (
+    <div className="min-h-screen bg-grey-900 text-white flex flex-col items-center p-10">
+      <h1 className="text-4xl font-bold mb-6 text-center text-cyan-400">User Management System</h1>
+
+      <UserForm addUser={addUser} />
+
+      <input
+        type="text"
+        placeholder="Search by name or email"
+        className="border border-grey-600 bg-grey-800 text-white p-3 rounded-lg  mt-4 mb-4 w-full focus:outline-none focus:border-cyan-4000"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <table className="mt-6 w-full border border-grey-700 rounded-lg overflow-hidden">
+        <thead>
+          <tr className="bg-cyan-600 text-white">
+            <th className="border p-2 cursor-pointer" onClick={sortUsers}>
+              Name
+            </th>
+            <th className="border p-2">Email</th>
+            <th className="border p-2">Role</th>
+            <th className="border p-2">Status</th>
+            <th className="border p-2">Created Date</th>
+            <th className="border p-2">Action</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {users
+            .filter(
+              (user) =>
+                user.name.toLowerCase().includes(search.toLowerCase()) ||
+                user.email.toLowerCase().includes(search.toLowerCase()),
+            )
+            .map((user, index) => (
+              <tr key={index} className="hover:bg-grey-800 transition">
+                <td className="border p-2">{user.name}</td>
+                <td className="border p-2">{user.email}</td>
+                <td className="border p-2">{user.role}</td>
+                <td className="border p-2">{user.status}</td>
+                <td className="border p-2">{user.createdDate}</td>
+                <td className="border p-2">
+                  <button
+                    className="bg-red-500 hover:bg-red-600 px-3 py-1 rounded text-white"
+                    onClick={() => deleteUser(index)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
---
+++ components/UserForm.tsx
@@ -0,0 +1,86 @@
+"use client"
+import{useState} from "react"
+
+
+
+export default function UserForm({addUser}:any) {
+  const[name,setName] = useState("");
+  const[email,setEmail] = useState("");
+  const[role,setRole] = useState("Admin");
+  const[status,setStatus]= useState("Active");
+
+  const handleSubmit = (e:any)=>{
+    e.preventDefault()
+    const newUser ={
+      name,
+      email,
+      role,
+      status,
+      createdDate:new Date().toLocaleDateString()
+    }
+    addUser(newUser)
+  }
+
+  return (
+
+    <div className="border p-4 mt-6 w-96">
+
+      <h2 className="text-xl font-semibold mb-4">
+        Add User
+      </h2>
+
+      <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+
+        <input
+          type="text"
+          placeholder="Full Name"
+          className="border p-2"
+          value={name}
+          onChange={(e)=>setName(e.target.value)}
+          required
+        />
+
+        <input
+          type="email"
+          placeholder="Email"
+          className="border p-2"
+          value={email}
+         onChange={(e)=>setEmail(e.target.value)}
+         required
+
+
+        />
+
+        <select
+         className="border p-2 bg grey-900 text-white"
+          value={role}
+          onChange={(e)=>setRole(e.target.value)}
+          className ="border border-gray-600 bg-gray-800 text-white p-3 rounded-lg "
+         >
+
+          <option>Admin</option>
+          <option>User</option>
+          <option>Manager</option>
+        </select>
+
+        <select 
+        className="border p-2"
+        value={status}
+        onChange={(e)=>setStatus(e.target.value)}
+         className ="border border-gray-600 bg-gray-800 text-white p-3 rounded-lg "
+          >
+          <option>Active</option>
+          <option>Inactive</option>
+          
+        </select>
+
+        <button type="submit" className="bg-blue-500 text-white p-2">
+          Add User
+        </button>
+
+      </form>
+
+    </div>
+
+  )
+}
